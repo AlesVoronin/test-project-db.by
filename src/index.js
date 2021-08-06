97fa7b2c61c9d600c0db005b27e0b4f4b47143a1
AlesVoronin <voronin@mkts.by>
2021-08-06 15:07:12 +0300

    feat: add media query for mobile device

--- src/index.js
+++ src/index.js
@@ -2,5 +2,21 @@ import './styles/index.scss';
 import './assets/fonts/Roboto-Regular.ttf';
 import './component.js';
 
+const burger_button = document.querySelector(".burger-menu");
+const mobile_menu = document.querySelector(".mobile-navbar-container")
+//const navigation_menu = document.getElementsByClassName("navigation_menu");
 
+
+
+function burger_active(){
+  if(mobile_menu.classList.contains("mobile-navbar-container_act") == true){
+    mobile_menu.classList.remove("mobile-navbar-container_act");
+  }
+  else{
+    mobile_menu.classList.add("mobile-navbar-container_act");
+  }
+}
+
+
+burger_button.addEventListener("mousedown", burger_active);
 
